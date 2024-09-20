261b7bd09616c72c858ac96b821d7c05be6c953a
Rio Harley <136496249+Dev-Harley@users.noreply.github.com>
2024-09-20 18:05:37 +0000

    conversionOpearation-P2

--- 03/conversionOperator.js
+++ 03/conversionOperator.js
@@ -1,35 +1,63 @@
-let score = 12
+// let score = 12
 
-let score2 ="22abc"
+// let score2 ="22abc"
 
-console.log(typeof score2)
+// console.log(typeof score2)
 
-let valueInNumber = Number(score2)
+// let valueInNumber = Number(score2)
 
-console.log(typeof valueInNumber)
+// console.log(typeof valueInNumber)
 
-console.log(valueInNumber)
+// console.log(valueInNumber)
 
 //"33" => 33 Easy
 // "33abc" => NaN (Not a number whne console.log but type sis number)
 
 //true => 1 ; false => 0
 
-let isPlaying = 1
+// let isPlaying = 1
 
-let booleanIsPlaying = Boolean(isPlaying)
+// let booleanIsPlaying = Boolean(isPlaying)
 
-console.log(typeof isPlaying)
-console.log(booleanIsPlaying)
+// console.log(typeof isPlaying)
+// console.log(booleanIsPlaying)
 
 // 1=> true ; 0 => falso 
 // "" => false (when empty is false )
 // "Valorant " => true
 
 
-let someNumber = 33
+// let someNumber = 33
 
-let StringNumber = String(someNumber)
+// let StringNumber = String(someNumber)
 
-console.log(StringNumber)
-console.log(typeof StringNumber)
+// console.log(StringNumber)
+// console.log(typeof StringNumber)
+
+// *** Operations ** //
+
+let Price = 5999
+let discountPrice = -Price
+console.log(discountPrice)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%2)
+console.log(10**3)
+
+
+let str1 = "Rio"
+let str2 = " Harley"
+
+// let str3 = str1 + str2 // Concatination
+console.log(str1 + str2)
+
+console.log( "1" + 2)
+console.log( 1 + "2")
+console.log ("1" + "2" + 8)
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2") 
+
+f
