8e9fa22bfc5f375ff158d8532ebed6d456751965
Rio Harley <136496249+Dev-Harley@users.noreply.github.com>
2024-09-20 17:31:32 +0000

    dataTypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 1445563
+let accountEmail = "workatks70@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+
+let accountState;
+
+
+//accountId = 2 // Not Allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "987654"
+accountCity = "Mumbai"
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
---
+++ 02_dataTypes/dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all Js code as a newer version
+
+//  alert("3+3") We ar using Node JS not Browser
+
+console.log(69 + 1);
+console.log("RioHarley");
+ 
+
+let Game = "Valorant"
+let Playtime = 9
+let isPlaying = true
+let KD = 9.22
+
+console.table( [Game, Playtime, isPlaying, KD])
+
+
+// number => 2 to power 53
+// bigint {big number values }
+// string = ""
+//boolean => true/false
+//null => standalone value
+//undefinedd => Empty
+// Symbol => Unique
+
+console.log(typeof isPlaying)
