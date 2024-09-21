e37a893b09e7046d9c810a188a0fc457c32b3830
Rio Harley <136496249+Dev-Harley@users.noreply.github.com>
2024-09-21 17:16:30 +0000

    added dates lecture

---
+++ 01_Basics/07_dates.js
@@ -0,0 +1,17 @@
+// Dates
+
+// let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+// console.log(typeof myDate)
+
+// const myBday = new Date(2004, 0,7)
+
+// console.log(myBday.toDateString())
+
+// console.log(myBday.toLocaleString())
+
+let myTimestamp = Date.now()
+
+console.log(myTimestamp)
