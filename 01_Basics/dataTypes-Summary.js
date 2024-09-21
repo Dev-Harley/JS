46f232ee80f52fe8abf6be1dcaf46e709ed07502
Rio Harley <136496249+Dev-Harley@users.noreply.github.com>
2024-09-21 09:23:06 +0000

    learnedMemory

--- 01_Basics/dataTypes-Summary.js
+++ 01_Basics/dataTypes-Summary.js
@@ -39,3 +39,4 @@ console.log("Hello Dawg")
 }
 
 console.log(typeof myfunction)
+
---
+++ 01_Basics/memory.js
@@ -0,0 +1,27 @@
+// Stack memory and Heap Memory
+
+// Stack memory is used in primitive 
+
+//Heap Memory is used in Non-Primitive
+
+let myIGN ="Rio Harley"
+
+let mobileIGN = myIGN
+
+mobileIGN = "Harley 07"
+
+console.log(myIGN)
+console.log(mobileIGN)
+
+
+let user1 = {
+    email: "harley07@gmail.com",
+    UPI:"harley@ybl"
+}
+
+let user2 = user1
+
+user2.email = "rio07@gmail.com"
+
+console.log(user1.email);
+console.log(user2.email);
