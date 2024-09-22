a1a8fcea68fc8ef54b9fc87d178283bc0c1a4e33
Rio Harley <136496249+Dev-Harley@users.noreply.github.com>
2024-09-22 11:50:27 +0000

    Module-2

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,31 @@
+
+
+// const ranks =["Silver", "Gold,", "Plat", "Diamond"]
+
+// const myArr2 = new Array(8384097023,8130516668)
+// console.log(myArr[7])
+
+// console.table(myArr)
+// console.log(myArr2)
+// console.table(myArr2)
+
+// myArr.push("Harley")
+// myArr.push("07")
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr.includes('Rio'))
+// console.log(myArr.indexOf('Rio'))
+
+// const newArr = myArr.join()
+// console.log(myArr)
+
+// console.log(newArr)
+// console.log(typeof newArr)
+const myArr =[0,1,2,3,4,5,]
+
+const slicedArr = myArr.slice(1,3)
+const splicedArr = myArr.splice(1,3)
+
+console.log(myArr)
+console.log(slicedArr)
+console.log(splicedArr)
